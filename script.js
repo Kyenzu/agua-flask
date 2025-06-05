673557260b55871329c414777ed2126b3e1a03fd
Kyenzu <sunadixs99@gmail.com>
2025-06-05 21:43:43 +0800

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById('predictionForm').addEventListener('submit', async funct
 
 
     try {
-        const response = await fetch('http://localhost:8000/api/hfp_prediction', {
+        const response = await fetch('https://agua-flask.onrender.com/hfp_prediction', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ inputs: [inputData] })
